eda8b11e5432f7e03620f976e7b5ad8dfd616570
maciekadamczykk <maciekadamczyk.opole28@gmail.com>
2025-05-11 17:30:02 +0200

    One-hot encoding and label encoding

--- main.py
+++ main.py
@@ -7,6 +7,23 @@ df = pd.read_csv("Data.csv")
 
 
 x = df.iloc[:,:-1].values
-y = df.iloc[:,-1]
+y = df.iloc[:,-1].values
+
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(x[:,1:])
+x[:,1:] = imputer.transform(x[:,1:])
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])],remainder="passthrough")
+x = np.array(ct.fit_transform(x))
 
 print(x)
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+print(x)
+print(y)
