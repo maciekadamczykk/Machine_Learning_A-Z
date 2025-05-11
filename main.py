547984abd6e1d32696ae513a59d167b28e8ec8c2
maciekadamczykk <maciekadamczyk.opole28@gmail.com>
2025-05-11 20:09:21 +0200

    Feature scaling

--- main.py
+++ main.py
@@ -20,10 +20,19 @@ from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])],remainder="passthrough")
 x = np.array(ct.fit_transform(x))
 
-print(x)
+
 
 le = LabelEncoder()
 y = le.fit_transform(y)
 
-print(x)
-print(y)
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2, random_state= 1) 
+
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train[:,3:] = sc.fit_transform(x_train[:,3:])
+x_test[:,3:] = sc.transform(x_test[:,3:])
+
+print(x_train)
+print(x_test)
