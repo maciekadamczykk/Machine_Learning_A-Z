b129095e1278d219729a6a17fb941525ade6591d
maciekadamczykk <maciekadamczyk.opole28@gmail.com>
2025-05-20 11:37:49 +0200

    Polynomial regression model and predictions

--- polynomial.py
+++ polynomial.py
@@ -1,8 +1,30 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
-
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 
 df = pd.read_csv('Position_Salaries.csv')
 
-print(df.head(20))
+X = df.iloc[:,1:-1].values
+y = df.iloc[:,-1].values
+
+
+lin_reg = LinearRegression()
+lin_reg.fit(X,y)
+
+pol_reg = PolynomialFeatures(degree=4)
+X_poly = pol_reg.fit_transform(X)
+
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly,y)
+
+plt.title("Polynomial Regression")
+plt.scatter(X,y, color="red")
+plt.plot(X, lin_reg_2.predict(X_poly), color="blue")
+plt.xlabel("Level")
+plt.ylabel("Salary")
+plt.show()
+
+print(lin_reg_2.predict(pol_reg.fit_transform([[6.5]])))
