8d1c0f145bd23e04fe0db5f1f52a0dacd26b879b
maciekadamczykk <maciekadamczyk.opole28@gmail.com>
2025-05-26 11:43:10 +0200

    Predicting new result

--- svr.py
+++ svr.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
 
 df = pd.read_csv('Position_Salaries.csv')
 
@@ -19,3 +20,8 @@ y = sc2.fit_transform(y)
 
 print(X)
 print(y)
+
+regressor = SVR(kernel= "rbf")
+regressor.fit(X,y)
+
+sc2.inverse_transform(regressor.predict(sc.transform([[6.6]])).reshape(-1,1))
