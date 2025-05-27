c7550946acdef2ae97692ed49384fb58ea5add98
maciekadamczykk <maciekadamczyk.opole28@gmail.com>
2025-05-27 11:04:07 +0200

    Changing decision tree into random forest

--- random_forest.py
+++ random_forest.py
@@ -8,10 +8,11 @@ df = pd.read_csv('Position_Salaries.csv')
 X = df.iloc[:,1:-1].values
 y = df.iloc[:,-1].values
 
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 
-regressor = DecisionTreeRegressor(random_state=42)
+regressor = RandomForestRegressor(n_estimators=10, random_state=42)
 
 regressor.fit(X,y) 
 
 print(regressor.predict([[6.5]]))
+
