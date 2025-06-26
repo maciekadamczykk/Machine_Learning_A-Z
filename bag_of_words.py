dc4f43fcea688ca032e01b543252f9d175950b10
maciekadamczykk <maciekadamczyk.opole28@gmail.com>
2025-06-26 12:31:18 +0200

    nlp preprocess

--- bag_of_words.py
+++ bag_of_words.py
@@ -4,7 +4,18 @@ import pandas as pd
 df = pd.read_csv("Restaurant_Reviews.tsv", delimiter="\t", quoting=3)
 
 import re
-import nltk 
+import nltk
+import ssl
+
+# Fix SSL certificate issue on macOS
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+# Download stopwords
 nltk.download("stopwords") 
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
@@ -15,3 +26,9 @@ for i in range(0,len(df)):
     review = re.sub('[^a-zA-Z]', " ", df["Review"][i])
     review = review.lower()
     review = review.split()
+    ps = PorterStemmer() 
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words("english"))] 
+    review = ' '.join(review) 
+    corpus.append(review)
+
+print(nltk.data.path)
