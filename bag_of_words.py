f320c566d90075b532f02f5b8ef8abfcfaeb6b59
maciekadamczykk <maciekadamczyk.opole28@gmail.com>
2025-06-26 15:07:40 +0200

    nb nlp and logistic regression nlp

--- bag_of_words.py
+++ bag_of_words.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("Restaurant_Reviews.tsv", delimiter="\t", quoting=3)
 
@@ -35,10 +36,37 @@ for i in range(0,len(df)):
 
 from sklearn.feature_extraction.text import CountVectorizer
 
-cv = CountVectorizer(max_features=1500)
+cv = CountVectorizer(max_features=1200)
 
 X = cv.fit_transform(corpus).toarray()
 y = df.iloc[:,-1].to_numpy()
 
+#Naive Bayes
 
-print(len(X[0]))
+from sklearn.model_selection import train_test_split
+
+X_test, X_train, y_test, y_train = train_test_split(X,y,test_size=0.2,random_state=28)
+
+from sklearn.naive_bayes import GaussianNB
+
+nb = GaussianNB()
+
+nb.fit(X_train,y_train)
+y_pred = nb.predict(X_test) 
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+acc = accuracy_score(y_test,y_pred)
+print(acc)
+import seaborn as sns
+
+from sklearn.linear_model import LogisticRegression
+
+regressor = LogisticRegression()
+regressor.fit(X_train,y_train)
+y_pred2 = regressor.predict(X_test)
+
+c_matrix = confusion_matrix(y_test,y_pred2)
+acc2 = accuracy_score(y_test,y_pred2)
+print(acc2)
+sns.heatmap(c_matrix,annot=True)
+plt.show()
